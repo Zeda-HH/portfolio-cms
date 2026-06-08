b23391962efa132872b0422ea187c63ceb5f1943
zemen <helisozemen148@gmail.com>
2026-06-08 18:43:22 +0300

    Force fresh migration on startup

--- start.sh
+++ start.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-php artisan migrate:fresh --force
+php artisan migrate:fresh --force --seed
+php artisan config:clear
+php artisan cache:clear
 php artisan serve --host=0.0.0.0 --port=$PORT
