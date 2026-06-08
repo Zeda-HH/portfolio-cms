c81d111c12ea9d1a6b366f56d50a51fa6ab63a05
zemen <helisozemen148@gmail.com>
2026-06-08 17:28:48 +0300

    Fix database migration

--- start.sh
+++ start.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 php artisan migrate --force
 php artisan serve --host=0.0.0.0 --port=$PORT
+
+#!/bin/bash
+php artisan migrate:fresh --force
+php artisan serve --host=0.0.0.0 --port=$PORT
